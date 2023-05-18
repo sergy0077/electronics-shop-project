9813be825c9d479c309707ec9563c6be6288cb21
Sergy007 <kutsenkosergey@mail.ru>
2023-05-18 19:01:58 +0200

    Создан экземпляр класса item, поля и функции.

--- src/item.py
+++ src/item.py
@@ -5,26 +5,41 @@ class Item:
     pay_rate = 1.0
     all = []
 
+    @classmethod
+    def clear_all(cls):
+        """
+        Очищает список всех товаров.
+        """
+        cls.all = []
+
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
         Создание экземпляра класса item.
-
         :param name: Название товара.
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        pass
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        Item.all.append(self)  # Добавляем текущий объект в список all
+
+
 
     def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
-
         :return: Общая стоимость товара.
         """
-        pass
+        return self.price * self.quantity
+
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        if self.name == "Смартфон":
+            self.price = self.price * self.pay_rate
+        else:
+            self.price = self.price
+

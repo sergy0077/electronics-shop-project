6a4ccdd8fa43beddd2d7959321725085e98e9a7e
Sergy007 <kutsenkosergey@mail.ru>
2023-05-18 18:58:55 +0200

    test_item.py::test_calculate_total_price PASSED                          [ 33%] test_item.py::test_apply_discount PASSED                                 [ 66%] test_item.py::test_all_items PASSED                                      [100%] tests\test_item.py ...                                                                                                                           [100%]
    ---------- coverage: platform win32, python 3.11.3-final-0 -----------
    Name              Stmts   Miss  Cover
    -------------------------------------
    src\__init__.py       0      0   100%
    src\item.py          17      2    88%
    -------------------------------------
    TOTAL                17      2    88%

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,40 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+import pytest
+
+def test_calculate_total_price():
+    item1 = Item("Смартфон", 10000, 20)
+    item2 = Item("Ноутбук", 20000, 5)
+
+    assert item1.calculate_total_price() == 200000
+    assert item2.calculate_total_price() == 100000
+
+def test_apply_discount():
+    item1 = Item("Смартфон", 10000, 20)
+    item2 = Item("Ноутбук", 20000, 5)
+
+    Item.pay_rate = 0.8
+    item1.apply_discount()
+
+    assert item1.price == 8000.0
+    assert item2.price == 20000
+    assert item1.price == 10000 * Item.pay_rate
+
+def test_all_items(monkeypatch):
+    # Создаем объекты Item
+    item1 = Item("Смартфон", 10000, 20)
+    item2 = Item("Ноутбук", 20000, 5)
+
+    # Monkey patching - временно изменяем атрибут all класса Item
+    monkeypatch.setattr(Item, 'all', [item1, item2])
+
+    # Проверяем, что список all содержит два элемента
+    assert len(Item.all) == 2
+
+    # Проверяем, что элементы в списке all соответствуют созданным объектам Item
+    assert Item.all[0] == item1
+    assert Item.all[1] == item2
+
+
+if __name__ == "__main__":
+    pytest.main()

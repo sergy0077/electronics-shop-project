e8467023e8ffb026c9ce99a1a7aa7c4de04bfe4f
Sergy007 <kutsenkosergey@mail.ru>
2023-06-02 23:44:44 +0200

    - Создан класс Keyboard для товара “клавиатура” с атрибутом language и методом для изменения языка  - Создан класс класс-миксин в цепочку наследования класса Keyboard.  - два языка: EN и RU. Изменяем язык методом change_lang().

---
+++ src/keyboard.py
@@ -0,0 +1,40 @@
+from src.item import Item
+
+
+class LanguageMixin:
+    def __init__(self):
+        self._language = "EN"
+
+    @property
+    def language(self):
+        return self._language
+
+    @language.setter
+    def language(self, value):
+        if value not in ["EN", "RU"]:
+            raise ValueError("Invalid language. Supported languages: EN, RU")
+        self._language = value
+
+    def change_lang(self):
+        if self._language == "EN":
+            self._language = "RU"
+        else:
+            self._language = "EN"
+        return self
+
+
+class Keyboard(Item, LanguageMixin):
+    def __init__(self, name, price, quantity):
+        super().__init__(name, price, quantity)
+        self._language = "EN"
+
+    def __repr__(self):
+        return f"Keyboard('{self.name}', {self.price}, {self.quantity})"
+
+    def __str__(self):
+        return self.name
+
+
+
+
+

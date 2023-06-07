6ae46b7c6ebc5379794c57d4a3e6aa9318326e7c
Sergy007 <kutsenkosergey@mail.ru>
2023-06-08 00:54:44 +0300

    Доработал код в классе Keyboard, взаимодействие с раскладкой теперь в миксине, в том числе атрибут language. Удалил лишний код

--- src/keyboard.py
+++ src/keyboard.py
@@ -14,14 +14,21 @@ class LanguageMixin:
     def change_lang(self):
         if self._language == "EN":
             self._language = "RU"
+            return self
         else:
             self._language = "EN"
-        return self
+            return self
 
 
 class Keyboard(LanguageMixin, Item):
+    @property
+    def language(self):
+        return self._language
+
     def __init__(self, name, price, quantity):
         super().__init__(name, price, quantity)
+
+
 
 
 
